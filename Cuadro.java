785f003cb62f29259200609462b4c296e3da4273
Huguintoch <huguintoch@users.noreply.github.com>
2019-11-02 21:29:17 -0600

    Virus Iterativo

---
+++ Cuadro.java
@@ -0,0 +1,18 @@
+
+public class Cuadro {
+
+	private int state;
+	
+	public Cuadro() {
+		this.state = 0;
+	}
+	
+	public int getState() {
+		return this.getState();
+	}
+	
+	public void setState(int state) {
+		this.state = state;
+	}
+	
+}
--- Prueba.java
+++ Prueba.java
@@ -1,10 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
-public class Prueba extends JFrame {
+public class Prueba extends JPanel implements Runnable{
 
 	private final int WINDOW_WIDTH,
-			  		  WINDOW_HEIGHT;
+			  		  WINDOW_HEIGHT,
+			  		  DELAY;
+	
+	private Thread animator;
 	
 	private int cellSize,
 				cols,
@@ -15,11 +19,11 @@ public class Prueba extends JFrame {
 	public Prueba() {
 		super();
 		
-		this.WINDOW_WIDTH = 1077;
-		this.WINDOW_HEIGHT = 721;
+		this.WINDOW_WIDTH = 1080;
+		this.WINDOW_HEIGHT = 720;
+		this.DELAY = 1000;
 		
 		this.setSize(this.WINDOW_WIDTH, this.WINDOW_HEIGHT);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		this.cellSize = 10;
 		this.cols = this.WINDOW_WIDTH/this.cellSize;
@@ -27,13 +31,21 @@ public class Prueba extends JFrame {
 		
 		this.grid = new int[this.cols][this.rows];
 		
+		this.grid[70][35] = 1;
+		
+		this.animator = new Thread(this);
+	    this.animator.start();
+		
 		this.setVisible(true);
-		this.setResizable(false);
 	}
 	
 	public void paint(Graphics g) {
+		super.paint(g);
+		this.paintVirus(g);
+	}
+	
+	public void paintVirus(Graphics g) {
 		int x, y;
-		updateGrid(6, this.cols/2, this.rows/2);
 		for (int i = 0; i < this.cols; i++) {
 			for (int j = 0; j < this.rows; j++) {
 				if(grid[i][j] == 1) {
@@ -41,35 +53,65 @@ public class Prueba extends JFrame {
 				} else {
 					g.setColor(new Color(240, 240, 240));
 				}
-				x = 8+this.cellSize*i;
-				y = 12+this.cellSize*j;
+				x = this.cellSize*i;
+				y = this.cellSize*j;
 				g.fillRect(x, y, this.cellSize, this.cellSize);
 			}
 		}
-		
 	}
 	
-	public void updateGrid(int n, int x, int y) {
-		grid[x][y] = 1;
-		if(n > 1) {
-			if(grid[x+1][y] == 0) {
-				this.updateGrid(n-1, x+1, y);
-			}
-			if(grid[x-1][y] == 0) {
-				this.updateGrid(n-1, x-1, y);
-			}
-			if(grid[x][y+1] == 0) {
-				this.updateGrid(n-1, x, y+1);
-			}
-			if(grid[x][y-1] == 0) {
-				this.updateGrid(n-1, x, y-1);
-			}
+	public void updateGrid() {
+		ArrayList<int[]> cellsToChange = new ArrayList<>();
+		for(int i=1; i<this.cols-1; i++) {
+    		for(int j=1; j<this.rows-1; j++) {
+    			if(grid[i+1][j] == 1 || grid[i-1][j] == 1 || grid[i][j+1] == 1 || grid[i][j-1] == 1) {
+    				int[] toAdd = {i,j}; 
+    				cellsToChange.add(toAdd);
+    			}
+    		}
+    	}
+		for(int i=0; i<cellsToChange.size(); i++) {
+			this.grid[cellsToChange.get(i)[0]][cellsToChange.get(i)[1]] = 1;
 		}
 	}
 	
+
+    @Override
+    public void run() {
+
+        long beforeTime, timeDiff, sleep;
+        beforeTime = System.currentTimeMillis();
+
+        while (true) {
+
+            timeDiff = System.currentTimeMillis() - beforeTime;
+            sleep = DELAY - timeDiff;
+            if (sleep < 0) {
+                sleep = 2;
+            }
+            try {
+                Thread.sleep(sleep);
+                this.updateGrid();
+                this.repaint();
+            } catch (InterruptedException e) {
+            
+            }
+            beforeTime = System.currentTimeMillis();
+            
+        }
+    }
 	
 	public static void main(String[] args) {
+		
 		Prueba p = new Prueba();
+		
+		JFrame jf = new JFrame();
+		
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setSize(1077, 721);
+		jf.setVisible(true);
+		jf.add(p);
+
 	}
 
 }
