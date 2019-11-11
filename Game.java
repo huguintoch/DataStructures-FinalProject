3d5e992856bb9e17c945b7ddcc6a7465bf18b5aa
Huguintoch <huguintoch@users.noreply.github.com>
2019-11-10 18:52:08 -0600

    Cure and resources values tweaked

--- Game.java
+++ Game.java
@@ -72,7 +72,7 @@ public class Game extends JPanel implements Runnable, MouseListener, KeyListener
 		this.paintValues(g);
 	}
 
-	public void paintValues(Graphics g) {
+	private void paintValues(Graphics g) {
 		for (int i = 0; i < COLS; i++) {
 			for (int j = 0; j < ROWS; j++) {
 				g.setColor(Color.BLACK);
@@ -91,7 +91,7 @@ public class Game extends JPanel implements Runnable, MouseListener, KeyListener
 		}
 	}
 
-	public void paintVirus(Graphics g) {
+	private void paintVirus(Graphics g) {
 		int x, y;
 		g.setColor(new Color(0, 0, 0, 100));
 		for (int i = 0; i < COLS; i++) {
@@ -105,7 +105,7 @@ public class Game extends JPanel implements Runnable, MouseListener, KeyListener
 		}
 	}
 
-	public void paintWalls(Graphics g) {
+	private void paintWalls(Graphics g) {
 		g.setColor(new Color(0, 0, 255, 150));
 		int x, y;
 		int[][] cells;
@@ -120,21 +120,28 @@ public class Game extends JPanel implements Runnable, MouseListener, KeyListener
 		}
 	}
 
-	public void paintResources(Graphics g) {
+	private void paintResources(Graphics g) {
 		g.setColor(new Color(255, 255, 0, 170));
 		int cell[][];
 		int x, y;
 		for (int i = 0; i < this.resources.length; i++) {
 			cell = this.resources[i].getGridCells();
 			for (int j = 0; j < cell.length; j++) {
-				x = CELL_SIZE*cell[j][0];
-				y = CELL_SIZE*cell[j][1];
-				g.fillRect(x, y, CELL_SIZE, CELL_SIZE);
+				x = cell[j][0];
+				y = cell[j][1];
+				updateResources(x, y);
+				g.fillRect(CELL_SIZE*x, CELL_SIZE*y, CELL_SIZE, CELL_SIZE);
 			}
 		}
 	}
 
-	public void paintCollectors(Graphics g) {
+	private void updateResources(int x, int y) {
+		if(grid[x][y] == 0) {
+			grid[x][y] = -2;
+		}
+	}
+	
+	private void paintCollectors(Graphics g) {
 		int x, y;
 		int[][] cells;
 
