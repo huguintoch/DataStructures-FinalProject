ea397f6d52ffe658872e2101653b3a616bc42d8b
Memohto <42613170+Memohto@users.noreply.github.com>
2019-11-13 13:05:27 -0600

    Constructor implementado

---
+++ Constructor.java
@@ -0,0 +1,56 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Constructor implements Runnable {
+	
+	private final int DELAY = 50;
+	private int x,
+				y;
+	
+	private Thread hilo;
+	
+	private boolean done = false;
+	
+	public Constructor(int x, int y, int type) {
+		this.x = x;
+		this.y = y;
+		this.hilo = new Thread(this);
+		this.hilo.start();
+	}
+	
+	public void paintConstructor(Graphics g) {
+		g.setColor(Color.CYAN);
+		g.fillRect(this.x, this.y, Game.CELL_SIZE, Game.CELL_SIZE);
+	}
+	
+	public boolean isDead() {
+		return this.done;
+	}
+
+	@Override
+	public void run() {
+		long beforeTime, timeDiff, sleep;
+        beforeTime = System.currentTimeMillis();
+        int cont = 0;
+
+		while(true) {
+			timeDiff = System.currentTimeMillis() - beforeTime;
+            sleep = this.DELAY - timeDiff;
+            if (sleep < 0) {
+                sleep = 2;
+            }
+
+            try {
+				Thread.sleep(sleep);
+				cont++;
+				if(cont%30 == 0) {
+					this.done = true;
+				}
+			} catch (InterruptedException e) {
+
+			}
+            beforeTime = System.currentTimeMillis();
+		}
+	}
+	
+}
