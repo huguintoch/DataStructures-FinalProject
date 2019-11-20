df35f71107ce46ff2574b5b0b4be734d891c7dcb
Huguintoch <huguintoch@users.noreply.github.com>
2019-11-19 19:43:46 -0600

    Constructor animation

--- Constructor.java
+++ Constructor.java
@@ -1,5 +1,7 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 
 import javax.swing.CellEditor;
@@ -11,13 +13,26 @@ public class Constructor implements Runnable {
 	
 	private int x,
 				y,
-				type;
+				type,
+				progressBar;
 	
 	private Game game;
 	
 	private Thread hilo;
 	
-	private Image sprite = new ImageIcon("constructor.png").getImage();
+	private Image sprite = new ImageIcon("constructor.png").getImage(),
+			      dustSprite;
+	
+	private Image[] dustsSprites = {new ImageIcon("Layer 1.png").getImage(),
+								    new ImageIcon("Layer 2.png").getImage(),
+								    new ImageIcon("Layer 3.png").getImage(),
+								    new ImageIcon("Layer 4.png").getImage(),
+								    new ImageIcon("Layer 5.png").getImage(),
+								    new ImageIcon("Layer 6.png").getImage(),
+								    new ImageIcon("Layer 7.png").getImage(),
+								    new ImageIcon("Layer 8.png").getImage(),
+								    new ImageIcon("Layer 9.png").getImage(),
+								    new ImageIcon("Layer 10.png").getImage()};
 	
 	private boolean done = false;
 	
@@ -25,6 +40,7 @@ public class Constructor implements Runnable {
 		this.x = x;
 		this.y = y;
 		this.type = type;
+		this.progressBar = 0;
 		this.game = game;
 		this.hilo = new Thread(this);
 		if(start) {
@@ -32,6 +48,7 @@ public class Constructor implements Runnable {
 		}
 		int[] cell = {x, y};
 		this.game.setGrid(cell, -7);
+		this.dustSprite = this.dustsSprites[0];
 	}
 	
 	public void start() {
@@ -39,7 +56,12 @@ public class Constructor implements Runnable {
 	}
 	
 	public void paint(Graphics g) {
+		g.drawImage(this.dustSprite, this.x*Game.CELL_SIZE-8, this.y*Game.CELL_SIZE+12, 35, 15, game);
 		g.drawImage(this.sprite,this.x*Game.CELL_SIZE, this.y*Game.CELL_SIZE, game);
+		Graphics2D g2 = (Graphics2D) g;
+	    g2.setStroke(new BasicStroke(2));
+		g2.setColor(Color.GREEN);
+		g2.drawLine(this.x*Game.CELL_SIZE, this.y*Game.CELL_SIZE+22, x*Game.CELL_SIZE+this.progressBar, y*Game.CELL_SIZE+22);
 	}
 	
 	public boolean isDead() {
@@ -51,6 +73,7 @@ public class Constructor implements Runnable {
 		long beforeTime, timeDiff, sleep;
         beforeTime = System.currentTimeMillis();
         int cont = 0;
+        int animIndex = 0;
 
 		while(true) {
 			timeDiff = System.currentTimeMillis() - beforeTime;
@@ -61,6 +84,19 @@ public class Constructor implements Runnable {
             try {
 				Thread.sleep(sleep);
 				cont++;
+				
+				if(cont%2 == 0) {
+					this.dustSprite = this.dustsSprites[animIndex++];
+				}
+				
+				if(cont%20 == 0) {
+					animIndex = 0;
+				}
+				
+				if(cont%10 == 0) {
+					this.progressBar+=2;
+				}
+				
 				if(cont%100 == 0 && !this.done) {
 					this.done = true;
 					if(type == 1) {
